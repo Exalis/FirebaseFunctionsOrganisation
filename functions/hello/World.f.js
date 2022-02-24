4d937fb54a75d97e5e34af9f373275737ffffd47
Exalis <acornu004@gmail.com>
2022-02-24 14:17:15 +0100

    init project

--- functions/hello/World.f.js
+++
@@ -1,7 +0,0 @@
-import * as functions from 'firebase-functions'
-
-export default functions.region("europe-west3").https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
-  });
- 
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
-'use strict';
-const loadFunctions = require('firebase-function-tools')
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-const config = functions.config().firebase
+const functions = require("firebase-functions");
 
-admin.initializeApp(config)
-loadFunctions(__dirname, exports, true)
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
